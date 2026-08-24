00c95111f88e0cd81d1cd6276111c84468671b76
Mod Machinery <github@dinocovelli.com>
2026-08-24 00:22:57 -0400

    updates

--- dnf-autoupdate.sh
+++ dnf-autoupdate.sh
@@ -1,11 +1,12 @@
 #!/bin/env bash
-#
+
 ## 1. Save this script to: /usr/local/bin/
 ## 2. Edit crontab and add the line without '#' (or devise your own schedule):
-##    sudo nano /etc/crontab
+##    This example runs every Sunday at 7:07 AM local time.
+##
+# sudo nano /etc/crontab
+## Add (remove '#'' to uncomment):
 #7 7 * * sun	root		/usr/local/bin/dnf-autoupdate > /dev/null 2>&1
-#
-## This example runs every Sunday at 7:07 AM local time.
 
 dnf update -qy
 dnf upgrade -qy
